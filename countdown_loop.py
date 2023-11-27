752a49505369a88b8c35efd46d5fb3ccd056484d
Airlair <144745830+Airlair@users.noreply.github.com>
2023-11-27 12:22:56 -0500

    Add files via upload

---
+++ countdown_loop.py
@@ -0,0 +1,30 @@
+def while_counter(num):
+    pass
+num = 0
+counter = 10
+while counter >= num:
+    print (counter)
+    counter -= 1
+
+   
+
+def for_counter(num):
+    pass
+for counter in range (10, -1, -1):
+    print(counter)
+    
+
+
+
+
+def main():
+    counter = 10
+    while_counter(counter)
+    for_counter(counter)
+    
+
+    
+
+
+if __name__ == "__main__":
+    main()
